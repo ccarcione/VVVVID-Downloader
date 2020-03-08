1ab990d9739f33dffe159be9d443a18d17ee0bf5
Carcione Christian <carcione.christian@gmail.com>
2020-03-08 03:32:32 +0100

    add empty console app

---
+++ VVVVID-Downloader.ConsoleApp/VVVVID-Downloader.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VVVVID_Downloader.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
